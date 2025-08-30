887caca02f8913f6d19d74232ef58317a4e8a1b2
nayoung16 <nykim1016@sookmyung.ac.kr>
2025-08-30 17:44:01 +0900

    fix : cors error

--- src/main/java/com/example/startlight/config/SecurityConfig.java
+++ src/main/java/com/example/startlight/config/SecurityConfig.java
@@ -52,9 +52,11 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(Arrays.asList("http://localhost:3000","http://localhost:8080",
-                "http://" + awsApiUrl + ":3000",
-                "http://" + awsApiUrl + ":8080"));
+        configuration.setAllowedOrigins(Arrays.asList(
+                "http://localhost:3000",
+                "http://localhost:8080",
+                awsApiUrl + ":3000",
+                awsApiUrl + ":8080"));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.addAllowedMethod("*");
         configuration.setAllowedHeaders(Arrays.asList("Authorization", "Cache-Control", "Content-Type","X-Requested-With"));
