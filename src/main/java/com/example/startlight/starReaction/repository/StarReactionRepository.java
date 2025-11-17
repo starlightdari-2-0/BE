eabe94f02d1829206e2632b47b7014bc0bb6d5a8
nayoung16 <nykim1016@sookmyung.ac.kr>
2025-11-17 15:19:00 +0900

    feat(service) : MemoryStar 좋아요 추가, 삭제

--- src/main/java/com/example/startlight/starReaction/repository/StarReactionRepository.java
+++ src/main/java/com/example/startlight/starReaction/repository/StarReactionRepository.java
@@ -10,12 +10,13 @@ import java.util.List;
 import java.util.Optional;
 
 public interface StarReactionRepository extends JpaRepository<StarReaction, Long> {
-    // 한 글에서 한 유저의 특정 타입 좋아요 한 개
-//    Optional<StarReaction> findByMemoryStar_Memory_idAndMember_Member_idAndReactionType(
-//            Long memoryStarId,
-//            Long userId,
-//            ReactionType reactionType
-//    );
+
+    @Query("select s from StarReaction s where s.memoryStar.memory_id = :memoryStarId and s.member.member_id = :userId and s.reactionType = :reactionType")
+    Optional<StarReaction> findByMemoryIdAndMemberIdAndReactionType(
+            @Param("memoryStarId") Long memoryStarId,
+            @Param("userId") Long userId,
+            @Param("reactionType") ReactionType reactionType
+    );
 
     @Query("select s from StarReaction s where s.memoryStar.memory_id = :memoryStarId and s.member.member_id = :userId")
     List<StarReaction> findByMemoryIdAndMemberId(@Param("memoryStarId") Long memoryStarId, @Param("userId") Long userId);
