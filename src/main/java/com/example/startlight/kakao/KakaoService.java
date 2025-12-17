3e7f330d2e966e77ff7d11a13f7f14183f519c3b
nayoung16 <nykim1016@sookmyung.ac.kr>
2025-12-17 20:52:04 +0900

    fix(service) : kakao login redirect url 변경

--- src/main/java/com/example/startlight/kakao/KakaoService.java
+++ src/main/java/com/example/startlight/kakao/KakaoService.java
@@ -127,6 +127,6 @@ public class KakaoService {
     }
 
     public String getMyPageUrl() {
-        return awsUrl + "/mypage/myInfo";
+        return "https://starlightdari.site/mypage/myInfo";
     }
 }
